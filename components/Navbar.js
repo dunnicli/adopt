1a6a937401321fa726931e3ff833d24c63e5dada
John Dunnicliffe <dunnicli@gmail.com>
2023-01-12 20:50:23 -0600

    cats and dogs

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,6 +15,16 @@ function Navbar() {
   <li className="-mb-px mr-1">    
     <Link className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="/">Home</Link>
   </li>
+  <li className="-mb-px mr-1">    
+    <Link className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="/shelterapi/dogs">Dogs</Link>
+  </li>
+  <li className="-mb-px mr-1">    
+    <Link className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="/shelterapi/cats">Cats</Link>
+  </li>
+  <li className="-mb-px mr-1">    
+    <Link className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="/shelterapi">All Animals</Link>
+  </li>
+  
   <li className="mr-1">
   <Link className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" href="/notes">Notes</Link>
   </li>
